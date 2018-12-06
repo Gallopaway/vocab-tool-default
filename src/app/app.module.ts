81a18f505157a86342e7758132694121b2d48ba4
Michael Galloway <magallopaway@gmail.com>
2018-12-06 17:39:42 +1300

    removed angular routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchPipe } from './search.pipe';
 import { FilterPipe } from './filter.pipe';
 import { VocabComponent } from './vocab/vocab.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { FilterAlphabetPipe } from './filter-alphabet.pipe';
 
@@ -18,13 +16,11 @@ import { FilterAlphabetPipe } from './filter-alphabet.pipe';
     SearchPipe,
     FilterPipe,
     VocabComponent,
-    SideNavComponent,
     QuizComponent,
     FilterAlphabetPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
